962e542b2ac0c87a80af596fdf8ba0368869d1cc
Rob Parker <into.the.parker@gmail.com>
2018-10-11 07:25:53 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,20 @@
+class Song 
+  
+  attr_accessor :name, :artist, :genre
+  
+  @@count = 0
+  
+  def initialize
+    @@count += 1
+    
+  end  
+  
+  def self.count 
+    @@count
+  end  
+  
+  def self.genre
+      
+  end
+  
+end
